74a9c04e858f6b4c8f89f7c990808883f53e9910
RefinedStone <chem.en.9273@gmail.com>
2025-05-14 01:27:38 +0900

    feat: v2 instruction 추가

--- src/main/kotlin/com/akradev/bloggen/controller/ExampleController.kt
+++ src/main/kotlin/com/akradev/bloggen/controller/ExampleController.kt
@@ -1,8 +1,12 @@
 package com.akradev.bloggen.controller
 
+import org.springframework.ai.chat.messages.SystemMessage
 import org.springframework.ai.chat.messages.UserMessage
 import org.springframework.ai.chat.prompt.Prompt
 import org.springframework.ai.ollama.OllamaChatModel
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,15 +19,28 @@ data class ChatReply(val reply: String)
 @RequestMapping("/api/chat")
 class ChatController(
     private val chatModel: OllamaChatModel,
+    @Autowired private val env: Environment,
 ) {
 
-    @PostMapping
-    fun chat(@RequestBody request: ChatRequest): Any {
+    @PostMapping("/v1")
+    fun chatV1(@RequestBody request: ChatRequest): Any {
         val userMessage = UserMessage(request.prompt)
         val prompt = Prompt(userMessage)
         val temp = chatModel.call(prompt).results.map { response ->
             response.output
         }
         return temp
+    }
+
+    @PostMapping("/v2")
+    fun chatV2(@RequestBody request: ChatRequest): Any {
+        val instruction = env.getProperty("instruction.v1") ?: ""
+        val userMessage = UserMessage(request.prompt + SystemMessage(instruction))
+        val prompt = Prompt(listOf(SystemMessage(instruction), userMessage))
+
+        val response = chatModel.call(prompt)
+        val reply = response.results.joinToString("\n") { it.output.toString() }
+
+        return ChatReply(reply)
     }
 }
